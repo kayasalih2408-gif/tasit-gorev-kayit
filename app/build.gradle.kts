f4fe5df0a3c6e3de6b629869dadb04d6aeffade3
kayasalih2408-gif <kaya.salih2408@gmail.com>
2026-08-14 14:47:15 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,11 +58,7 @@ android {
   }
 }
 
-// Configure the Secrets Gradle Plugin to use .env and .env.example files
-// to match the convention used in Web projects.
-secrets {
-  propertiesFileName = ".env"
-  defaultPropertiesFileName = ".env.example"
+
   ignoreList.add("FIREBASE_APPCHECK_DEBUG_TOKEN")
 }
 
